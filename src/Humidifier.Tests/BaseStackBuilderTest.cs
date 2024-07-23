b06f4a36e24ab5630dfc084a8c44ef921d4ec8b8
Ron Sijm <sijm.ron+github@gmail.com>
2024-07-23 20:42:11 +0200

    - Convert "TestConsole" to actual xunit tests, since it's for testing purposes.  -- Allows easier refactoring while verifying that nothing breaks

---
+++ src/Humidifier.Tests/BaseStackBuilderTest.cs
@@ -0,0 +1,23 @@
+﻿using Humidifier.Json;
+
+namespace Humidifier.Tests
+{
+    public abstract class BaseStackBuilderTest
+    {
+        [Fact]
+        public async Task CanBuildStack()
+        {
+            var stack = await BuildStack();
+
+            var serializer = new JsonStackSerializer();
+            var template = serializer.Serialize(stack);
+
+            var settings = new VerifySettings();
+            settings.UseDirectory("VerificationResults");
+
+            await Verify(template, extension: "json", settings);
+        }
+
+        protected abstract Task<Stack> BuildStack();
+    }
+}
